ca8036172c17149ecf98e55866a16b0f3293098b
Rune Kaagaard <rumi.kg@gmail.com>
2022-08-21 18:03:36 +0200

    Work on example code.

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.formats import date_format
 
 def full_name(user):
     return f"{user.first_name} {user.last_name}"
@@ -11,8 +10,8 @@ class Timeslot(models.Model):
     booked_to = models.ForeignKey("auth.User", on_delete=models.CASCADE, default=None, null=True, related_name="+")
 
     def __repr__(self):
-        d1 = date_format(self.start, format='SHORT_DATETIME_FORMAT', use_l10n=True)
-        d2 = date_format(self.end, format='SHORT_DATETIME_FORMAT', use_l10n=True)
+        d1 = str(self.start)[:16]
+        d2 = str(self.end)[:16]
         booked_to = f' booked_to="{full_name(self.booked_to)}"' if self.booked_to else " booked_to=None"
 
         return f'<Timeslot id={self.pk} start="{d1}" end="{d2}" doctor="{full_name(self.doctor)}"{booked_to}>'
