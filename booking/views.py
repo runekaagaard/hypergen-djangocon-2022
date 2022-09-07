4ad5f7cb3cd86ab692e65a075ee20ca7aa5196de
Rune Kaagaard <rumi.kg@gmail.com>
2022-09-07 15:09:36 +0200

    good stuff.

--- booking/views.py
+++ booking/views.py
@@ -2,46 +2,19 @@ from contextlib import contextmanager
 from itertools import groupby
 
 from hypergen.imports import *
+from hypergen.plugins.alertify import AlertifyPlugin, alertify_messages
 
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 from booking.models import Timeslot
 
-def hprint(*args, **kwargs):
-    # TODO: Move this upstream.
-    # TODO: Make custom support for:
-    #           - Queryset
-    #           - values and values_list
-    #           - query (sql)
-    #           - grouper
-    from pprint import pformat
-    d = dict
-
-    @component
-    def typeinfo(x):
-        span(" (", x.__class__.__module__, ".", type(x).__name__, ")", style=d(color="darkgrey"))
-
-    def fmt(x):
-        pre(code(pformat(x, width=120)))
-
-    with div(style=d(padding="8px", margin="4px 0 0 0", background="#ffc", color="black",
-                     font_family="sans-serif")):
-        if len(args) == 1 and not kwargs:
-            div(typeinfo(args[0]))
-            fmt(args[0])
-        else:
-            for i, arg in enumerate(args, 1):
-                div(b("arg", i, sep=" "), typeinfo(arg))
-                fmt(arg)
-
-        for k, v in kwargs.items():
-            div(b(k), typeinfo(v))
-            fmt(v)
+### Templates ###
 
 def menu():
     with nav(class_="tabs nav-center"):
-        a("Book timeslot", href=book_timeslot.reverse(), class_="active")
-        a("My bookings", href=my_bookings.reverse())
+        a("Book timeslot", href=book_timeslot.reverse(), class_active="active")
+        a("My bookings", href=my_bookings.reverse(), class_active="active")
 
     p(" ")
 
@@ -74,26 +47,8 @@ def timeslot_template(timeslots_by_date, query=""):
                         td(timeslot.fmt_time)
                         td(timeslot.doctor)
                         td(
-                            button("Book", id=("book-timeslot", timeslot.pk),
+                            button("Book", class_="button primary", id=("book-timeslot", timeslot.pk),
                                    onclick=callback(book_timeslot2, query, timeslot.pk)))
-
-@liveview(perm="booking.create_booking", base_template=base_template)
-def book_timeslot(request):
-    timeslots_by_date = Timeslot.available_by_date()
-    timeslot_template(timeslots_by_date)
-
-@action(perm="booking.create_booking", target_id="target")
-def filter_timeslots(request, query):
-    timeslots_by_date = Timeslot.available_by_date(query)
-    timeslot_template(timeslots_by_date)
-
-@action(perm="booking.create_booking", target_id="target")
-def book_timeslot2(request, query, timeslot_id):
-    command("alert", f"You booked a new time. Good stuff.")
-    Timeslot.objects.filter(pk=timeslot_id).update(booked_to=request.user)
-
-    timeslots_by_date = Timeslot.available_by_date(query)
-    timeslot_template(timeslots_by_date)
 
 @contextmanager
 def card(title, description):
@@ -103,15 +58,43 @@ def card(title, description):
         with footer(class_="is-right"):
             yield
 
-@liveview(perm="booking.view_booking", base_template=base_template)
+### Liveviews ###
+
+liveview_settings = dict(base_template=base_template, user_plugins=[AlertifyPlugin()])
+
+@liveview(perm="booking.create_booking", **liveview_settings)
+def book_timeslot(request):
+    timeslots_by_date = Timeslot.available_by_date()
+    timeslot_template(timeslots_by_date)
+
+@liveview(perm="booking.view_booking", **liveview_settings)
 def my_bookings(request):
     with div(class_="row"):
         for timeslot in Timeslot.objects.filter(booked_to=request.user).order_by("start"):
             with div(class_="col-4"), card(timeslot.fmt_datetime, timeslot.doctor):
                 a("Cancel", class_="button error", id=("timeslot", timeslot.pk),
-                  onclick=callback(cancel_timeslot, timeslot.pk, confirm_="Are you sure?"))
+                  onclick=callback(cancel_timeslot, timeslot.pk, confirm="Are you sure?"))
+
+### Actions ###
+
+@action(perm="booking.create_booking", target_id="target")
+def filter_timeslots(request, query):
+    timeslots_by_date = Timeslot.available_by_date(query)
+    timeslot_template(timeslots_by_date)
+
+@action(perm="booking.create_booking", target_id="target")
+def book_timeslot2(request, query, timeslot_id):
+    messages.add_message(request, messages.SUCCESS, "You booked a new time. Good stuff <3")
+    messages.add_message(request, messages.INFO, "See your booked timeslots on this page!")
+    Timeslot.objects.filter(pk=timeslot_id).update(booked_to=request.user)
+
+    timeslots_by_date = Timeslot.available_by_date(query)
+    timeslot_template(timeslots_by_date)
+
+    # return HttpResponseRedirect(my_bookings.reverse())
+    command("hypergen.redirect", my_bookings.reverse())
 
 @action(perm="booking.create_booking", target_id="target", base_view=my_bookings)
 def cancel_timeslot(request, timeslot_id):
-    command("alert", f"Time is gone forever!")
+    messages.add_message(request, messages.WARNING, "The timeslot is GONE!")
     Timeslot.objects.filter(pk=timeslot_id).update(booked_to=None)
