7a11fcf7c47ccf5508a96e6d7c66eba9f1d7c0d3
Rune Kaagaard <rumi.kg@gmail.com>
2022-09-12 11:53:41 +0200

    Ready for practice.

--- booking/views.py
+++ booking/views.py
@@ -13,90 +13,33 @@ from booking.models import Timeslot
 
 @contextmanager
 def base_template():
-    doctype()
-    with head():
-        link(href=static("chota.css"))
-    with body(), div(class_="container"):
-        h1("Doctor booking")
-        with div(id="target"):
-            yield
+    yield
 
 base_template.target_id = "target"
 
 def free_template(timeslots_by_date):
-    menu()
-
-    with p():
-        input_(placeholder="Search here", id_="search", oninput=callback(search, THIS, debounce=50))
-
-    with table(class_="striped"):
-        thead(tr(th(x) for x in ("Date", "Time", "Doctor", "Actions")))
-        for date, timeslots in timeslots_by_date:
-            tr(th(date, colspan=4))
-            for timeslot in timeslots:
-                with tr():
-                    td()
-                    td(timeslot.fmt_time)
-                    td(timeslot.doctor)
-                    with td():
-                        button("Book", id_=["timeslot", timeslot.pk], class_="button primary",
-                               onclick=callback(book, timeslot.pk))
-
-def menu():
-    with nav(class_="tabs nav-center"):
-        a("Book timeslot", href=free.reverse(), class_active="active")
-        a("My bookings", href=booked.reverse(), class_active="active")
-
-@contextmanager
-def row():
-    with div(class_="row"):
-        yield
-
-@contextmanager
-def col(n):
-    with div(class_=f"col-{n}"):
-        yield
-
-@contextmanager
-def card(title, description):
-    with div(class_="card"):
-        header(h4(title))
-        p(description)
-        with footer(class_="is-right"):
-            yield
+    pass
 
 ### Liveviews ###
 
-@liveview(perm="booking.view_booking", base_template=base_template, user_plugins=[AlertifyPlugin()])
+@liveview(perm="booking.view_booking", base_template=base_template)
 def free(request):
-    timeslots_by_date = Timeslot.free_by_date()
-    free_template(timeslots_by_date)
+    pass
 
-@liveview(perm="booking.view_booking", base_template=base_template, user_plugins=[AlertifyPlugin()])
+@liveview(perm="booking.view_booking", base_template=base_template)
 def booked(request):
-    menu()
-
-    with row():
-        for timeslot in Timeslot.objects.filter(booked_to=request.user):
-            with col(4), card(timeslot, timeslot.doctor):
-                button("Cancel", id=("timeslot", timeslot.pk), class_="button error",
-                       onclick=callback(cancel, timeslot.pk, confirm="Are your sure?"))
+    pass
 
 ### Actions ###
 
-@action(perm="booking.view_booking", target_id="target", user_plugins=[AlertifyPlugin()])
+@action(perm="booking.view_booking", target_id="target")
 def search(request, query):
-    timeslots_by_date = Timeslot.free_by_date(query)
-    free_template(timeslots_by_date)
+    pass
 
-@action(perm="booking.add_booking", target_id="target", user_plugins=[AlertifyPlugin()])
+@action(perm="booking.add_booking", target_id="target")
 def book(request, pk):
-    Timeslot.book(request, pk)
-    messages.add_message(request, messages.SUCCESS, "You got it!")
+    pass
 
-    return HttpResponseRedirect(booked.reverse())
-
-@action(perm="booking.delete_booking", target_id="target", base_view=booked, user_plugins=[AlertifyPlugin()])
+@action(perm="booking.delete_booking", target_id="target")
 def cancel(request, pk):
-    messages.add_message(request, messages.WARNING, "Your booking is gone forever!")
-    Timeslot.cancel(request, pk)
+    pass
