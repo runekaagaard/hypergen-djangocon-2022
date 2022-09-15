eb4d42c9eae58435880f60714cf1fe76481265e5
Rune Kaagaard <rumi.kg@gmail.com>
2022-09-15 16:06:06 +0200

    New practice run

--- past/views.py
+++ past/views.py
@@ -1,7 +1,5 @@
 from contextlib import contextmanager
 
-from django.http.response import HttpResponse
-
 from hypergen.imports import *
 from hypergen.plugins.alertify import AlertifyPlugin
 
@@ -19,7 +17,6 @@ def base_template():
     with html():
         with head():
             link(href=static("chota.css"))
-            title("Doctor booking")
         with body():
             with div(class_="container"):
                 h1("Doctor booking")
@@ -27,23 +24,6 @@ def base_template():
                     yield
 
 base_template.target_id = "target"
-
-def free_template(timeslots_by_date):
-    menu()
-    with p():
-        input_(placeholder="Search here", id_="search", oninput=callback(search, THIS, debounce=50))
-    with table():
-        thead(tr(th(x) for x in ["Date", "Time", "Doctor", "Action"]))
-        for date, timeslots in timeslots_by_date:
-            tr(th(date, colspan=4))
-            for timeslot in timeslots:
-                with tr():
-                    td()
-                    td(timeslot.fmt_time)
-                    td(timeslot.doctor)
-                    with td():
-                        button("Book", id_=["timeslot", timeslot.pk], class_="button primary",
-                               onclick=callback(book, timeslot.pk))
 
 def menu():
     with nav(class_="tabs nav-center"):
@@ -68,6 +48,27 @@ def card(title, description):
         with footer(class_="is-right"):
             yield
 
+def free_template(timeslots_by_date):
+    menu()
+
+    with p():
+        input_(placeholder="Search here", id_="search", oninput=callback(search, THIS, debounce=50))
+
+    with table():
+        with thead():
+            tr(th(x) for x in ["Date", "Time", "Doctor", "Action"])
+        with tbody():
+            for date, timeslots in timeslots_by_date:
+                tr(th(date, colspan=4))
+                for timeslot in timeslots:
+                    with tr():
+                        td()
+                        td(timeslot)
+                        td(timeslot.doctor)
+                        with td():
+                            button("Book", id_=["timeslot", timeslot.pk], class_="button primary",
+                                   onclick=callback(book, timeslot.pk))
+
 ### Liveviews ###
 
 @liveview(perm="booking.view_booking", base_template=base_template, user_plugins=[AlertifyPlugin()])
@@ -80,25 +81,26 @@ def booked(request):
     menu()
     with row():
         for timeslot in Timeslot.objects.filter(booked_to=request.user):
-            with col(4), card(timeslot, timeslot.doctor):
-                button("Cancel", class_="button error", id_=["timeslot", timeslot.pk],
-                       onclick=callback(cancel, timeslot.pk, confirm="Are you sure?"))
+            with col(4), card(timeslot.fmt_datetime, timeslot.doctor):
+                button("Cancel", id=("timeslot", timeslot.pk), class_="button error",
+                       onclick=callback(cancel, timeslot.pk, confirm="Are your sure?"))
 
 ### Actions ###
 
-@action(perm="booking.view_booking", target_id="target", user_plugins=[AlertifyPlugin()])
+@action(perm="booking.view_booking", base_template=base_template, user_plugins=[AlertifyPlugin()])
 def search(request, query):
     timeslots_by_date = Timeslot.free_by_date(query)
     free_template(timeslots_by_date)
 
-@action(perm="booking.add_booking", target_id="target", user_plugins=[AlertifyPlugin()])
+@action(perm="booking.add_booking", base_template=base_template, user_plugins=[AlertifyPlugin()])
 def book(request, pk):
     Timeslot.book(request, pk)
     messages.add_message(request, messages.SUCCESS, "You got it!")
 
     return HttpResponseRedirect(booked.reverse())
 
-@action(perm="booking.delete_booking", target_id="target", base_view=booked, user_plugins=[AlertifyPlugin()])
+@action(perm="booking.delete_booking", base_template=base_template, user_plugins=[AlertifyPlugin()],
+        base_view=booked)
 def cancel(request, pk):
     Timeslot.cancel(request, pk)
     messages.add_message(request, messages.WARNING, "Your booking is gone forever!")
